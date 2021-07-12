0167e6a2f6b0d16205430570057d949e01e2b93a
Lukas Kaufmann <travianluk@gmail.com>
2021-07-12 18:03:51 +0200

    Initial code base for Avalonia app

---
+++ DiscordFormatter/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscordFormatter.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
